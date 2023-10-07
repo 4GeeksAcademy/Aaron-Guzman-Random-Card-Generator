67d11f6bfc6b41056278bb100e00170799e2f941
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2023-10-07 18:05:46 +0000

    Random card by ElEiron

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  button();
+  document.querySelector(".boton").addEventListener("click", button);
 };
+function button() {
+  let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let suit = ["♦", "♥", "♠", "♣"];
+  let randomValue = Math.floor(Math.random() * value.length);
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  document.querySelector("#topSuit").innerHTML = suit[randomSuit];
+  document.querySelector("#bottomSuit").innerHTML = suit[randomSuit];
+  document.querySelector("#number").innerHTML = value[randomValue];
+  document.querySelector(".card").style.color =
+    randomSuit <= 1 ? "red" : "black";
+}
